5331ed41b2a05e64cbb868d44546c0d0e83b554f
Naldo <edinaldoandradeoficial@gmail.com>
2026-03-21 12:29:22 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", e=>{
+  e.waitUntil(
+    caches.open("exna-cache").then(cache=>{
+      return cache.addAll([
+        "index.html",
+        "style.css",
+        "app.js"
+      ])
+    })
+  )
+})
